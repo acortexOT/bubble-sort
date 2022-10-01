af52a82b3dc19c02047ca2acb4fa5293a04c31c2
acortexot <acortexot@gmail.com>
2022-10-01 19:28:01 -0300

    finish code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,15 +5,24 @@ def bubble_sort(array)
 a = 0
 b = 1
 # 1 'switch' boolean variable that changes after a swap
-is_running = true
-# iterate through with i and i+1 and compare values
-array.each_with_index do |item, index|
-  a = item
-  b = array[index + 1]
-
+total_loops = 0
+while total_loops < array.count
+  # iterate through with i and i+1 and compare values
+  array.each_with_index do |item, index|
+    a = item
+    b = array[index + 1]
+    next if b == nil
+    if a > b then
+      array[index] = b
+      array[index + 1] = a
+      end   
+  end
+  total_loops += 1
 end
+
 # do until switch is off
+p array
 end 
-bubble_sort([1,1,4,1,5,1,33,3])
+bubble_sort([4,3,78,2,0,2])
 
 
