cdf7a882487da533f888356c616907a3253b3f91
acortexot <acortexot@gmail.com>
2022-10-01 10:13:01 -0300

    Basic algorithm and start code

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+#bubble_sort.rb
+
+def bubble_sort(array)
+# 2 variables to hold active values
+a = 0
+b = 1
+# 1 'switch' boolean variable that changes after a swap
+is_running = true
+# iterate through with i and i+1 and compare values
+array.each_with_index do |item, index|
+  a = item
+  b = array[index + 1]
+
+end
+# do until switch is off
+end 
+bubble_sort([1,1,4,1,5,1,33,3])
+
+
